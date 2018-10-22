f0d44b5b2aeb98facf2b130127c6f269fe11bae4
DroidFreak32 <rushabshah32@gmail.com>
2018-10-23 03:15:57 +0530

    Program 8: SMS app code
    Signed-off-by: DroidFreak32 <rushabshah32@gmail.com>

--- app/src/main/java/com/example/student/mad_labapp/MainActivity.java
+++ app/src/main/java/com/example/student/mad_labapp/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.student.mad_labapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button send;
+
+    EditText num, msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        send = findViewById(R.id.send);
+
+        num = findViewById(R.id.number);
+        msg = findViewById(R.id.message);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = num.getText().toString();
+                String message = msg.getText().toString();
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phoneNumber));
+                intent.putExtra("sms_body", message);
+
+                startActivity(intent);
+
+            }
+        });
+
     }
+
 }
