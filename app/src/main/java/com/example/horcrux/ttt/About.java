c45c464256cfd7162cf19030bc8c9f02286c7683
DroidFreak32 <rushabshah32@gmail.com>
2018-11-05 02:30:13 +0530

    TicTacToad
    Signed-off-by: DroidFreak32 <rushabshah32@gmail.com>

---
+++ app/src/main/java/com/example/horcrux/ttt/About.java
@@ -0,0 +1,21 @@
+package com.example.horcrux.ttt;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.about, menu);
+        return true;
+    }
+}
