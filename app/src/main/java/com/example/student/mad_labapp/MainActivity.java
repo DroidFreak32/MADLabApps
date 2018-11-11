4fde5dd275610acd15cd8a827838f29926e89d0e
DroidFreak32 <rushabshah32@gmail.com>
2018-11-12 00:15:43 +0530

    Add code for refreshing layout using threads

--- app/src/main/java/com/example/student/mad_labapp/MainActivity.java
+++ app/src/main/java/com/example/student/mad_labapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.student.mad_labapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(100);
+
+                } catch (InterruptedException e) {
+                    Log.e(getPackageName(), "Exception in sleep");
+                } finally {
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+            }
+        }.start();
     }
 }
