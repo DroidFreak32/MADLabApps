c3f2e4e223febe18e17b8c1901c5714c12847289
DroidFreak32 <rushabshah32@gmail.com>
2018-11-12 01:57:50 +0530

    Add code for Battery broadcast receiver

--- app/src/main/java/com/example/student/mad_labapp/MainActivity.java
+++ app/src/main/java/com/example/student/mad_labapp/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.student.mad_labapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mBatteryLevelText;
+    ProgressBar mBatteryLevelProgress;
+    BroadcastReceiver mReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBatteryLevelText = findViewById(R.id.textView);
+        mBatteryLevelProgress = findViewById(R.id.progressBar);
+        mReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+                mBatteryLevelText.setText("Battery Level : " + level);
+                mBatteryLevelProgress.setProgress(level);
+                RelativeLayout rl = findViewById(R.id.rl);
+                if (level > 60)
+                    rl.setBackgroundColor(Color.GREEN);
+                else if (level > 30)
+                    rl.setBackgroundColor(Color.BLUE);
+                else
+                    rl.setBackgroundColor(Color.RED);
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
+
+    @Override
+    protected void onStop() {
+        unregisterReceiver(mReceiver);
+        super.onStop();
     }
 }
