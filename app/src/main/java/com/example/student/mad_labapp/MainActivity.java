14a798838122c5df30f9c5c1dbd8db31cbefd682
DroidFreak32 <rushabshah32@gmail.com>
2018-11-11 22:50:20 +0530

    Add code

--- app/src/main/java/com/example/student/mad_labapp/MainActivity.java
+++ app/src/main/java/com/example/student/mad_labapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.student.mad_labapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button colorButton;
+    RelativeLayout rl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        colorButton = findViewById(R.id.colorButton);
+        rl = findViewById(R.id.rl);
+        colorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random ranColor = new Random();
+                rl.setBackgroundColor(Color.rgb(
+                        ranColor.nextInt(256),  // R
+                        ranColor.nextInt(256),  // G
+                        ranColor.nextInt(256)   // B
+                ));
+            }
+        });
     }
 }
