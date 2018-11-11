304d27fbc4531557f0e4f3c0d7ebd10366c51de9
DroidFreak32 <rushabshah32@gmail.com>
2018-11-12 01:01:22 +0530

    Add code for Call using Intent

--- app/src/main/java/com/example/student/mad_labapp/MainActivity.java
+++ app/src/main/java/com/example/student/mad_labapp/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.student.mad_labapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button call;
+    EditText num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.CALL_PHONE}, 1);
+
+        call = findViewById(R.id.call);
+        num = findViewById(R.id.number);
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number = num.getText().toString();
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + number));
+                try {
+                    startActivity(callIntent);
+                } catch (Exception e) {
+                    Log.d("error", e.getMessage());
+                    Toast.makeText(getApplicationContext(), "call permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
     }
 }
