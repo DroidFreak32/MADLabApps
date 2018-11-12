1fb46291fd186f0e352658cc85ea7ea2ae824464
DroidFreak32 <rushabshah32@gmail.com>
2018-11-12 16:18:47 +0530

    Add code to change color

--- app/src/main/java/com/example/student/mad_labapp/MainActivity.java
+++ app/src/main/java/com/example/student/mad_labapp/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.student.mad_labapp;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
     Button closeButton;
     AlertDialog.Builder builder;
 
@@ -18,31 +22,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         closeButton = findViewById(R.id.button);
         builder = new AlertDialog.Builder(this);
-        closeButton.setOnClickListener(this);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.setMessage("Do you want to change the color ?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Toast.makeText(getApplicationContext(), "YES selected", Toast.LENGTH_SHORT).show();
+                                RelativeLayout rl = findViewById(R.id.rl);
+                                Random gen = new Random();
+                                rl.setBackgroundColor(Color.rgb(
+                                        gen.nextInt(256),
+                                        gen.nextInt(256),
+                                        gen.nextInt(256)
+                                ));
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // Action for 'NO' Button
+                                dialog.cancel();
+                                Toast.makeText(getApplicationContext(), "NO selected", Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+                //Creating dialog box
+                AlertDialog alert = builder.create();
+                //Setting the title manually
+                alert.setTitle("Color Changer");
+                alert.show();
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        builder.setMessage("Do you want to close this application ?")
-                .setCancelable(false)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        finish();
-                        Toast.makeText(getApplicationContext(), "YES selected", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // Action for 'NO' Button
-                        dialog.cancel();
-                        Toast.makeText(getApplicationContext(), "NO selected", Toast.LENGTH_SHORT).show();
 
-                    }
-                });
-        //Creating dialog box
-        AlertDialog alert = builder.create();
-        //Setting the title manually
-        alert.setTitle("AlertDialogExample");
-        alert.show();
-    }
 }
